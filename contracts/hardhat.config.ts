f6238b41bcedd7f908fd5093dfbed21c483ea34a
healthykim <bsbs8645@snu.ac.kr>
2023-01-09 17:54:35 +0900

    chore: Setup for testing

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,10 +1,27 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import 'solidity-docgen';
+import "hardhat-gas-reporter";
+
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
-  docgen: {}
+  solidity: {
+    version: "0.8.17",
+    settings: 
+      {
+        optimizer: 
+          {
+            enabled: true,
+            runs: 10000,
+          }
+      }
+  },
+  docgen: {},
+  gasReporter: {
+    enabled: true,
+    currency: "ETH",
+    //coinmarketcap: process.env.COINMARKETCAP_API_KEY, //to fetch gas data
+  }
 };
 
 export default config;

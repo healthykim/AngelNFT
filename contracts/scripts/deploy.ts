784d5877afd2b0255dc09316e0b5f5ac08061e70
healthykim <bsbs8645@snu.ac.kr>
2023-01-10 13:00:11 +0900

    chore: Setup for deployment

--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -5,9 +5,14 @@ async function main() {
   const AngelToken = await ethers.getContractFactory("AngelToken");
   const angelToken = await AngelToken.deploy();
 
+  const Donate = await ethers.getContractFactory("Donate");
+  const donate = await Donate.deploy(angelToken.address);
+
   await angelToken.deployed();
+  await donate.deployed();
 
   console.log(`Angel Token was deployed to ${angelToken.address}`);
+  console.log(`Donate was deployed to ${donate.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere

c94e385d903b1f4cf0354c017a3f2736af4a4659
AngelKim <67711171+healthykim@users.noreply.github.com>
2023-01-13 16:46:03 +0900

    [Feature] Implement function for listing user's NFT Images. (#16)
    * merge
    
    * wip
    
    * feat: Implement ui for listing owner's NFT Images
    
    * fix: Remove redundant logic

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -22,6 +22,13 @@ const config: HardhatUserConfig = {
     enabled: true,
     currency: "ETH",
     //coinmarketcap: process.env.COINMARKETCAP_API_KEY, //to fetch gas data
+  },
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/rkGCoetVrGUD3bVzI4JAvK8MahPmbj0x`,
+      accounts: ['6df173a0377142a373a474151da2e848e23afb9db170973b54ea8bccbbc0af96']
+    }
+    
   }
 };
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import MainPage from "./routes/main_page";
 import Layout from "./components/layout";
 import DonatePage from './routes/donate_page';
 import MintCompletePage from './routes/mint_complete_page';
+import MyPage from './routes/my_page';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Route path="/" element={<MainPage/>} />
       <Route path="/donate" element={<DonatePage/>} />
       <Route path="/show_NFT" element={<MintCompletePage/>} />
+      <Route path="/my_page" element={<MyPage/>} />
     </Routes>
     </Layout>
     </BrowserRouter>

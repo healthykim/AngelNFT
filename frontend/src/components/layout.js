12ab84b8cf611574332490769cd3421f94a81552
hanjoongyoo <hanjoongyoo@naver.com>
2023-01-14 21:26:24 +0900

    style: Change js file in frontend indent spaces from 4 to 2

--- frontend/src/components/layout.js
+++ frontend/src/components/layout.js
@@ -3,27 +3,27 @@ import { Link } from 'react-router-dom';
 
 
 function Layout({ children }) {
-    return (
-        <div className='w-ful'>
-            <div className='fixed w-full backdrop-blur'>
-                <nav className="flex h-16 justify-between items-center px-12">
-                    <div className="flex items-center">
-                        <Link to="/" className="text-black font-bold text-xl mr-4">Angel NFT</Link>
-                        <Link to="/donate" className="text-black mr-4">Donate</Link>
-                        <Link to="/trade" className="text-black mr-4">Trade</Link>
-                    </div>
-                    <Link to="/my_page/my_nft" className="flex items-center">
-                        <div className='w-10 h-10 rounded-full bg-black'>
-                        </div>
-                    </Link>
-                </nav>
+  return (
+    <div className='w-ful'>
+      <div className='fixed w-full backdrop-blur'>
+        <nav className="flex h-16 justify-between items-center px-12">
+          <div className="flex items-center">
+            <Link to="/" className="text-black font-bold text-xl mr-4">Angel NFT</Link>
+            <Link to="/donate" className="text-black mr-4">Donate</Link>
+            <Link to="/trade" className="text-black mr-4">Trade</Link>
+          </div>
+          <Link to="/my_page/my_nft" className="flex items-center">
+            <div className='w-10 h-10 rounded-full bg-black'>
             </div>
-            {/* <div className='h-16 bg-ukblue'></div> */}
-            <div className='flex-col w-full h-full justify-center items-center'>
-                {children}
-            </div>
-        </div>
-    );
+          </Link>
+        </nav>
+      </div>
+      {/* <div className='h-16 bg-ukblue'></div> */}
+      <div className='flex-col w-full h-full justify-center items-center'>
+        {children}
+      </div>
+    </div>
+  );
 }
 
 export default Layout;

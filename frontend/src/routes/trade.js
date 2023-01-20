b0553ee68dc06cd5e3adf895f42363b28b393e45
hanjoongyoo <hanjoongyoo@naver.com>
2023-01-20 16:11:51 +0900

    feat: Improve trade page ui

--- frontend/src/routes/trade.js
+++ frontend/src/routes/trade.js
@@ -13,22 +13,22 @@ function Trade() {
 
   const getAccount = async () => {
     try {
-        if (window.ethereum) {
-            const accounts = await window.ethereum.request({
-                method: 'eth_requestAccounts',
-            });
-            setAccount(accounts[0]);
-        }
-        else {
-            alert("Install Metamask!");
-        }
+      if (window.ethereum) {
+        const accounts = await window.ethereum.request({
+          method: 'eth_requestAccounts',
+        });
+        setAccount(accounts[0]);
+      }
+      else {
+        alert("Install Metamask!");
+      }
     }
     catch (error) {
-        console.error(error);
+      console.error(error);
     }
   }
 
-  const getExchangeableTokenId = async() => {
+  const getExchangeableTokenId = async () => {
     try {
       const tokens = await AngelTokenContract.methods.getExchangeableTokenData().call();
       setExchangeableTokens(tokens);
@@ -38,11 +38,11 @@ function Trade() {
     }
   }
 
-  const onClickImage = async(tokenId) => {
+  const onClickImage = async (tokenId) => {
     try {
       const owner = await AngelTokenContract.methods.ownerOf(tokenId).call()
       const isOwnerToken = owner.toUpperCase() === account.toUpperCase();
-      if(isOwnerToken) {
+      if (isOwnerToken) {
         alert("This NFT is already in your pocket.")
         return;
       }
@@ -54,7 +54,7 @@ function Trade() {
     setSelectedTokenId(tokenId);
   }
 
-  useEffect(()=>{    
+  useEffect(() => {
     getAccount();
     getExchangeableTokenId();
   }, [isLoading])
@@ -63,23 +63,25 @@ function Trade() {
     <div>
       <div>
         <div className="h-16"></div>
-        <div className="flex flex-col px-16 2xl:px-32 items-center">
-          <div className="text-3xl 2xl:text-5xl pb-8 2xl:pb-12 2xl:pt-4">
-            Trade
+        <div className="flex flex-col px-24 2xl:px-48 items-start">
+          <div className="flex flex-col gap-2 pt-2 pb-8">
+            <h3 className="text-3xl font-bold">How To Trade</h3>
+            <ul className="flex flex-col gap-1 list-disc list-outside pl-8 text-lg">
+              <li>The NFTs below are registered by other users for exchange.</li>
+              <li>By clicking on the desired NFT, you can request an exchange.</li>
+              <li>Once another user approves the exchange request, NFTs are automatically exchanged.</li>
+              <li>If you make multiple requests using one NFT, only the last request will be valid.</li>
+            </ul>
           </div>
-          The NFTs below are registered by other users for exchange.  <br />
-          By clicking on the desired NFT, you can request an exchange.  <br />
-          Once another user approves the exchange request, NFTs are automatically exchanged.  <br />
-          If you make multiple requests using one NFT, only the last request will be valid. <br />
           <div className="grid grid-cols-5 gap-x-10 gap-y-8 2xl:gap-x-20 2xl:gap-y-16 w-full">
             {
-              exchangeableTokens.map((token, i)=> {
+              exchangeableTokens.map((token, i) => {
                 return (
                   <img
-                  key={i}
-                  src={`https://gateway.ipfs.io/ipfs/${ipfsImageHash}/images/${token.tokenId}.png`}
-                  className="rounded-lg 2xl:rounded-2xl cursor-pointer w-full"
-                  onClick={() => { onClickImage(token.tokenId);}}>
+                    key={i}
+                    src={`https://gateway.ipfs.io/ipfs/${ipfsImageHash}/images/${token.tokenId}.png`}
+                    className="rounded-lg 2xl:rounded-2xl cursor-pointer w-full"
+                    onClick={() => { onClickImage(token.tokenId); }}>
                   </img>
                 )
               })
